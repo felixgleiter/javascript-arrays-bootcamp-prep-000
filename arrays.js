2fcad5a5fe0f0a0ea45f8393a9d375d524c640bf
Felix Godfree <felixfgodfree@gmail.com>
2018-08-27 10:08:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift();
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
@@ -30,7 +31,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop();
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
